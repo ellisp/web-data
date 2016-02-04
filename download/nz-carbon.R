e9352f09b4acd9ff377f1163a855573976be680e
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-02-05 07:37:16 +1300

    R script for downloading carbon price

--- download/nz-carbon.R
+++ download/nz-carbon.R
@@ -1,1 +1,12 @@
 # download New Zealand carbon unit price
+
+library(stringr)
+
+download.file("http://www.carbonnews.co.nz/", destfile = "tmp/carbonnews.html")
+
+cn <- readLines("tmp/carbonnews.html")
+pr <- str_match(cn, "Spot NZUs close at \\$[0-9]+\\.[0-9][0-9]")
+pr <- as.numeric(str_sub(pr[!is.na(pr)], 21))
+
+tmp <- data.frame(Day = Sys.Date(), NZU_Price = pr)
+write.table(tmp, file = "data/carbon_NZU.csv", row.names = FALSE, append = TRUE, col.names = FALSE, sep = ",")
